31f33cface96ecdb183ff2c8ec1ee80a47e26486
Denis Belozerov <molotov.ant@mail.ru>
2022-10-25 04:08:56 +0500

    рефакторинг

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -7,9 +7,9 @@ export default function Messages({ currentUser, messages, quote, setQuote }) {
       {messages.map((message, index) => {
         const isCurrentUser = currentUser === message.user;
         return (
-          <li key={index} className={isCurrentUser && "currentUserMessage"}>
+          <li key={index} className={isCurrentUser ? "currentUserMessage" : ""}>
             {!isCurrentUser && <span className="name">{message.user}: </span>}
-            <div>
+            <div className="messageQuote">
               {message.quote?.value && `В ответ на ${message.quote?.value}`}
             </div>
             <span>{message.value}</span>
